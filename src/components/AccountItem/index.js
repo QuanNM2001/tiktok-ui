52f6bebcf1e3db6045ac9a1ba9a8561c3b96963a
QuanNM2001 <minhquanqn2701@gmail.com>
2023-02-20 01:01:19 +0700

    Header 2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
+const cx = classNames.bind(styles)
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/05aa222706c452c8ad0ea7d2e719a745~c5_100x100.jpeg?x-expires=1676998800&x-signature=Ffn37OL1VyNTpOkdsCWYnHcv7yc%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('infor')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
